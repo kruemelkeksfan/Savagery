64668965a005d27130787f451e414d4c3f2ef6aa
naomig96 <a01506542>
2020-05-05 17:40:47 +0200

    var_dumps for debugging

--- PHP/MOGUL/HttpHelper.php
+++ PHP/MOGUL/HttpHelper.php
@@ -8,20 +8,19 @@ private $base_url = "http://localhost:8000/";
 function post($path, $data){
     $postdata = json_encode($data);
     $url = $this->base_url.$path;
-    var_dump($url);
+    var_dump($postdata);
     $curl = curl_init($url);
     curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
     curl_setopt($curl, CURLOPT_POST, true);
     curl_setopt($curl, CURLOPT_POSTFIELDS, $postdata);
-    curl_setopt($curl, CURLOPT_SSL_VERIFYHOST, 0);
-    curl_setopt($curl, CURLOPT_SSL_VERIFYPEER, 0);
-
-    var_dump($curl);
 
     $test = file_get_contents($this->base_url."api_test.php");
     var_dump($test);
 
     $json_response = curl_exec($curl);
+
+    echo "json-response";
+    var_dump($json_response);
 
     if (curl_errno($curl)) {
         print curl_error($curl);
