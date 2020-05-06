0b53b1acb708c48fc9df78766ba21188744557b6
naomig96 <a01506542>
2020-05-06 21:30:20 +0200

    api for recruitment

---
+++ api/Armies/post_split_army.php
@@ -0,0 +1,20 @@
+<?php
+header("Access-Control-Allow-Origin: *");
+header("Content-Type: application/json; charset=UTF-8");
+header("Access-Control-Allow-Methods: OPTIONS,GET,POST,PUT,DELETE");
+header("Access-Control-Max-Age: 3600");
+header("Access-Control-Allow-Headers: Content-Type, Access-Control-Allow-Headers, Authorization, X-Requested-With");
+
+
+include_once '../Database.php';
+
+$database = new Database();
+
+$data = json_decode(file_get_contents("php://input"), JSON_OBJECT_AS_ARRAY);
+
+if ($data['townname'] != "") {
+
+    $townname = $data['townname'];
+    //ToDo
+
+}
