464ff02d3d2a7534998670aa9b6dff7a217b0bae
naomig96 <a01506542>
2020-05-07 00:29:30 +0200

    hopefully fix EVERYTHING

--- api/api_test_post.php
+++ api/api_test_post.php
@@ -5,7 +5,7 @@ header("Access-Control-Allow-Methods: OPTIONS,GET,POST,PUT,DELETE");
 header("Access-Control-Max-Age: 3600");
 header("Access-Control-Allow-Headers: Content-Type, Access-Control-Allow-Headers, Authorization, X-Requested-With");
 
-$data = json_decode(file_get_contents("php://input"), JSON_OBJECT_AS_ARRAY);
+$data = json_decode(file_get_contents("php://input"), true);
 $data["neue"] = "Eingabe";
 
 echo json_encode($data);
