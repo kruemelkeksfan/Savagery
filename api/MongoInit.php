24ae082693e28865221c1494efcec936b1b5c834
kruemelkeksfan <a_maximilian@web.de>
2020-06-04 20:02:23 +0200

    Refactored MongoInit.php for Setting Migration.

--- api/MongoInit.php
+++ api/MongoInit.php
@@ -6,18 +6,20 @@ include_once 'Database.php';
 $database = new MongoDB();
 
 // Init Collections
-$database->new_collection('Settings');
+$database->new_collection('Balancesettings');
 $database->new_collection('Buildingtypes');
 $database->new_collection('Playerdata');
 
+//Helper for API calls
+$http = new HttpHelper();
+
 // Define Balance Settings
-$database->add_document('Settings',
-	array('Map_Size' => 100, 'Start_Gold' => 100, 'Start_Tax' => 5, 'Start_Population' => 5, 'Range_Multiplier' => 10, 'Upgrade_Multiplier' => 50));
+$http->get('Migration/get_migrate_settings.php');
 //Define Buildingtypes
-$database->add_document('Buildingtypes',
+/*$database->add_document('Buildingtypes',
 	array('Blacksmith' => array('Effect' => 'Increases the Defense of all Armies of this Town.', 'Cost' => '10', 'Maxworkers' => '6'),
     'Tavern' => array('Effect' => 'Increases Attack Strength of every Soldier of this Town.', 'Cost' => '20', 'Maxworkers' => '4'),
-    'Townhall' => array('Effect' => 'Home to the Mayor of the Town (You).', 'Cost' => '0', 'Maxworkers' => '1'));
+    'Townhall' => array('Effect' => 'Home to the Mayor of the Town (You).', 'Cost' => '0', 'Maxworkers' => '1'));*/
 
 /*if($success && $fillBalaceSettings && $fillTimetable && $peaceTreatySuccess){
     echo json_encode(
