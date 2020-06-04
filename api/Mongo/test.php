8c2d073186805468aa07bdeb878f9af53fcccd03
naomig96 <a01506542>
2020-06-04 22:54:36 +0200

    test modify array

--- api/Mongo/test.php
+++ api/Mongo/test.php
@@ -16,7 +16,7 @@ if (empty($db)){
 $db->add_document('test_col', array('test'=>'Hello World!', 'town'=>array('name'=>'my_town',
     'armies'=>array(array('armyname'=>'cattroopers', 'strength'=>5),array('armyname'=>'catguard', 'strength'=>15))), 'username'=>'stupidcat'));
 
-$db->update_field('test_col', array('test'=>'Hello World!'), array('town.armies.$name.strength'=>100), array('arrayFilters'=>array('town.armies.armyname'=>'cattroopers')));
+$db->update_field('test_col', array('test'=>'Hello World!'), array('town.armies.$name.strength'=>100), array('arrayFilters'=>array('name.armyname'=>'cattroopers')));
 $db->add_to_array('test_col', array('test'=>'Hello World!'), array('town.armies'=>array('armyname'=>'catbattallion', 'strength'=>150)));
 $db->add_field('test_col', array('test'=>'Hello World!'), array('town.password'=>'pwd'));
 $db->update_field('test_col', array('test'=>'Hello World!'), array('test'=>'Goodbye World!'));
