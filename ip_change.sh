69fd49a845e21b9a186dcbc194124b4dac910d16
naomig96 <a01506542>
2020-06-04 14:32:13 +0200

    better shellscript

--- ip_change.sh
+++ ip_change.sh
@@ -2,4 +2,6 @@
 
 ip = $(hostname -i | awk '{print $1}')
 
-sed -i -e "s/localhost/$ip/g" MOGUL/HttpHelper.php
+sed -i -e "s/localhost/$ip/g" PHP/MOGUL/HttpHelper.php
+
+sed -i -e "s/localhost/$ip/g" api/MongoDB.php
