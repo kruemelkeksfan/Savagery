80d32b4d40cfb0bd5ded1a66b61908fd7b6c8079
kruemelkeksfan <a_maximilian@web.de>
2020-06-06 01:17:10 +0200

    Added Bugs to Data Migration.

--- api/MongoInit.php
+++ api/MongoInit.php
@@ -4,24 +4,72 @@ include_once 'Database.php';
 include_once 'MongoDatabase.php';
 
 // Database Connection
-$database = new MongoDatabase();
+$sql = new Database();
+$mongo = new MongoDatabase();
 
-// Define Balance Settings
-$settingdata = (new Database())->query('SELECT settingname, value FROM BalanceSettings;', array());
+// Import Balance Settings
+$settingdata = $sql->query('SELECT settingname, value FROM BalanceSettings;', array());
 
 $settings = array();
 foreach($settingdata as $setting)
 {
-	$settings[$setting['settingname']] =  $setting['value'];
+	$settings[$setting['settingname']] = $setting['value'];
 }
 
-$db = new MongoDatabase();
-$result = $db->add_document('BalanceSettings', $settings);
-//Define Buildingtypes
-/*$database->add_document('Buildingtypes',
-	array('Blacksmith' => array('Effect' => 'Increases the Defense of all Armies of this Town.', 'Cost' => '10', 'Maxworkers' => '6'),
-    'Tavern' => array('Effect' => 'Increases Attack Strength of every Soldier of this Town.', 'Cost' => '20', 'Maxworkers' => '4'),
-    'Townhall' => array('Effect' => 'Home to the Mayor of the Town (You).', 'Cost' => '0', 'Maxworkers' => '1'));*/
+$result = $mongo->add_document('BalanceSettings', $settings);
+
+//Import Buildingtypes
+$buildingtypedata = $sql->query('SELECT buildingtypename, effect, cost, maxworkers FROM Buildingtypes;', array());
+
+$buildingtypes = array();
+foreach($buildingtypedata as $buildingtype)
+{
+	$buildingtypes[$buildingtype['buildingtypename']] =
+		array('Effect' => $buildingtype['effect'], 'Cost' => $buildingtype['cost'], 'Maxworkers' => $buildingtype['maxworkers']);
+}
+
+$result = $mongo->add_document('Buildingtypes', $buildingtypes);
+
+// Import Player Data
+$playerdata = $sql->query('SELECT Users.username, Users.password, Users.gold, Towns.townname, Towns.position, Towns.tax, Towns.population, FROM Users
+	INNER JOIN Towns ON Users.username = Towns.owner;', array());
+
+$players = array();
+foreach($playerdata as $player)
+{
+	$players[$player['username']] = array('Password' => $player['password'], 'Gold' => $player['gold']);
+	$players[$player['username']][$player['townname']] = array('Position' => $player['position'], 'Tax' => $player['tax'], 'Population' => $player['population']);
+	
+	$buildingdata = $sql->query('SELECT building_id, workers, level, buildingtype FROM Buildings WHERE town = :0;', array($player['townname']));
+	$players[$player['username']][$player['townname']]['Buildings'] = array();
+	foreach($buildingdata as $building)
+	{
+		$players[$player['username']][$player['townname']]['Buildings'][$building['building_id']] =
+			array('Workers' => $building['workers'], 'Level' => $building['level'], 'Buildingtype' => $building['buildingtype']);
+	}
+	
+	$armydata = $sql->query('SELECT army_id, armyname, strength FROM Armies WHERE town = :0;', array($player['townname']));
+	$players[$player['username']][$player['townname']]['Armies'] = array();
+	foreach($armydata as $army)
+	{
+		$players[$player['username']][$player['townname']]['Armies'][$building['army_id']] =
+			array('Armyname' => $building['armyname'], 'Strength' => $building['strength']);
+	}
+	
+	$players[$player['username']][$player['townname']]['Treaties'] = array();
+	$treatydata = $sql->query('SELECT user1, expiry_time FROM PeaceTreaty WHERE user2 = :0;', array($player['username']));
+	foreach($treatydata as $treaty)
+	{
+		$players[$player['username']][$player['townname']]['Treaties'][$treaty['user1']] = $treaty['expiry_time'];
+	}
+	$treatydata = $sql->query('SELECT user2, expiry_time FROM PeaceTreaty WHERE user1 = :0;', array($player['username']));
+	foreach($treatydata as $treaty)
+	{
+		$players[$player['username']][$player['townname']]['Treaties'][$treaty['user2']] = $treaty['expiry_time'];
+	}
+}
+
+$result = $mongo->add_document('BalanceSettings', $settings);
 
 /*if($success && $fillBalaceSettings && $fillTimetable && $peaceTreatySuccess){
     echo json_encode(
