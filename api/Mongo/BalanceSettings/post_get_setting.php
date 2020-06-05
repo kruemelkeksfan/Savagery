110f474f27a29f58e8c3cb1a1ab3b9ca0036106d
kruemelkeksfan <a_maximilian@web.de>
2020-06-05 14:01:41 +0200

    New test.

--- api/Mongo/BalanceSettings/post_get_setting.php
+++ api/Mongo/BalanceSettings/post_get_setting.php
@@ -17,10 +17,10 @@ if($data['settingname'] != "") {
     $settingname = $data['settingname'];
     $value = $database->find_document('BalanceSettings');//, [], array('projection'=>array('_id'=>0, $settingname=>1)));
 
-    echo json_encode(1/*$value*/); //returns not as array, but plain value!!S
+    echo json_encode(array(1=>1)/*$value*/); //returns not as array, but plain value!!S
 }
 // TODO: Delete
 else
 {
-	echo json_encode(2);
+	echo json_encode(array(2=>2));
 }
