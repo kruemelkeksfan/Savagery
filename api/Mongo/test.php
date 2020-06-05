05cb26c8ba3d4229f45ca6f340423441d1579dc2
naomig96 <a01506542>
2020-06-05 17:15:40 +0200

    test arrays

--- api/Mongo/test.php
+++ api/Mongo/test.php
@@ -18,7 +18,7 @@ $db->add_document('test_col', array('test'=>'Hello World!', 'town'=>array('name'
 
 $db->add_document('test_col', array('cat'=>'miau'));
 
-$db->update_array_field('test_col',array('test'=>'Hello World!'), array('town.armies.$[name].strength'=>100), array('name.armyname'=>'cattroopers'));
+$error=$db->update_array_field('test_col',array('test'=>'Hello World!'), array('town.armies.$[name].strength'=>100), array('name.armyname'=>'cattroopers'));
 //$db->update_field('test_col', array('test'=>'Hello World!'), array('town.armies.$[name].strength'=>100), array('arrayFilters'=>array('name'=>array('armyname'=>'cattroopers', 'strength'=>5))));
 $db->add_to_array('test_col', array('test'=>'Hello World!'), array('town.armies'=>array('armyname'=>'catbattallion', 'strength'=>150)));
 $db->add_field('test_col', array('test'=>'Hello World!'), array('town.password'=>'pwd'));
@@ -28,4 +28,4 @@ $db->delete_field('test_col', array('test'=>'Goodbye World!'), array('username'=
 $data = $db->find_document('test_col');
 $data2 = $db->find_document('test_col',['test'=>'Hello World!'], $options = array('projection'=>array('_id'=>0, 'town.armies'=>1)));
 
-echo json_encode(array('data'=>$data));
+echo json_encode(array('data'=>$data), array('data'=>$data2), ['error'=>$error]);
