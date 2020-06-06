925352fcb5e4f7068b8c0c8821b7ddc4979ae0d3
naomig96 <a01506542>
2020-06-06 12:31:08 +0200

    test settings and buildingtypes

--- PHP/login.php
+++ PHP/login.php
@@ -108,6 +108,7 @@ if(!empty($action))
     }
 	else if($action === 'init_Mongo'){
 	    //var_dump($http->post('Armies/post_get_army_by_town.php', array('username'=>'xxxNOOBxxx')));
+        var_dump($http->post('BalanceSettings/post_get_setting.php', array('settingname'=>'Map_Size')));
         var_dump($http->get('Buildingtypes/get_buildingtypes.php'));
         $result = $http->changeDB();
         //var_dump($result);
