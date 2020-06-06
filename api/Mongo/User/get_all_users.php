9a7c4f06f1e55c1f7f6a248675760552fbdb192a
naomig96 <a01506542>
2020-06-06 15:22:24 +0200

    test user migration

--- api/Mongo/User/get_all_users.php
+++ api/Mongo/User/get_all_users.php
@@ -2,7 +2,7 @@
 header('Access-Control-Allow-Origin: *');
 header('Content-Type: application/json');
 
-include_once '../Database.php';
+include_once '../../Database.php';
 
 $database = new MongoDatabase();
 
