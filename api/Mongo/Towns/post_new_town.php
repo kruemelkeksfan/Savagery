f143916ffadc3a47db697c857648c7c8656dd545
naomig96 <a01506542>
2020-06-07 22:29:00 +0200

    mongo new town

--- api/Mongo/Towns/post_new_town.php
+++ api/Mongo/Towns/post_new_town.php
@@ -6,39 +6,34 @@ header("Access-Control-Max-Age: 3600");
 header("Access-Control-Allow-Headers: Content-Type, Access-Control-Allow-Headers, Authorization, X-Requested-With");
 
 
-include_once '../Database.php';
+include_once '../../MongoDatabase.php';
 
-$database = new Database();
+$database = new MongoDatabase();
 
 $data = json_decode(file_get_contents("php://input"), true);
 
 if($data['username'] != "") {
 
     $username = $data['username'];
-    $mapsize = $database->query('SELECT value FROM BalanceSettings WHERE settingname=:0;', array('Map_Size'))[0]['value'];
+    $mapsize = $database->$database->find_document('BalanceSettings', [], array('projection'=>array('_id'=>0, 'Map_Size'=>1)))[0]['Map_Size'];
 
     if (empty($data['tax'])) {
-        $tax = $database->query('SELECT value FROM BalanceSettings WHERE settingname=:0;', array('Start_Tax'))[0]['value'];
+        $tax = $database->find_document('BalanceSettings', [], array('projection'=>array('_id'=>0, 'Start_Tax'=>1)))[0]['Start_Tax'];
     } else {
         $tax = $data['tax'];
     }
 
     if (empty($data['population'])){
-        $population = $database->query('SELECT value FROM BalanceSettings WHERE settingname=:0;', array('Start_Population'))[0]['value'];
+        $population = $database->find_document('BalanceSettings', [], array('projection'=>array('_id'=>0, 'Start_Population'=>1)))[0]['Start_Population'];
     } else {
         $population = $data['population'];
     }
 
 
-    if ($database->query('INSERT INTO Towns (townname, position, tax, population, owner) VALUES (:0, :1, :2, :3, :4);',
-        array($username . 's Town', mt_rand(0, $mapsize - 1), $tax, $population, $username))) {
+    $result = $database->update_field('Userdata', array('username'=>$username),
+        array('$set'=>array('townname'=>$username.'s Town', 'position'=>mt_rand(0, $mapsize - 1), 'tax'=>$tax, 'population'=>$population)));
+        echo json_encode(
+            array($result)
+        );
 
-        echo json_encode(
-            array('message' => 'Town Created', 'success' => true)
-        );
-    } else{
-        echo json_encode(
-            array('message' => 'Town Not Created', 'success' => false)
-        );
-    }
 }
