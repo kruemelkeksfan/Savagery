7ab2e9fcb8a58b9f5a3c0470c45ab037c4d1616e
naomig96 <a01506542>
2020-06-07 22:03:33 +0200

    mongo get gold

--- api/Mongo/User/post_get_gold.php
+++ api/Mongo/User/post_get_gold.php
@@ -6,9 +6,9 @@ header("Access-Control-Max-Age: 3600");
 header("Access-Control-Allow-Headers: Content-Type, Access-Control-Allow-Headers, Authorization, X-Requested-With");
 
 
-include_once '../Database.php';
+include_once '../../MongoDatabase.php';
 
-$database = new Database();
+$database = new MongoDatabase();
 
 $data = json_decode(file_get_contents("php://input"), true);
 
@@ -16,7 +16,7 @@ if($data['username'] != "") {
 
     $username = $data['username'];
 
-    $gold = $database->query("SELECT gold FROM Users WHERE username = :0;", array($username));
+    $gold = $database->find_document('Userdata',array('username'=>$username),array('$projection'=>array('$_id'=>0, 'gold'=>1)));
 
     echo json_encode($gold[0]);
 } else {
