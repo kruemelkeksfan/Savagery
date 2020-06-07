95bac3752a81a10d2549ad6b7eb3717fd6c4b6c7
naomig96 <a01506542>
2020-06-07 21:52:44 +0200

    mongo substract gold

--- api/Mongo/User/post_substract_gold.php
+++ api/Mongo/User/post_substract_gold.php
@@ -6,9 +6,9 @@ header("Access-Control-Max-Age: 3600");
 header("Access-Control-Allow-Headers: Content-Type, Access-Control-Allow-Headers, Authorization, X-Requested-With");
 
 
-include_once '../Database.php';
+include_once '../../MongoDatabase.php';
 
-$database = new Database();
+$database = new MongoDatabase();
 
 $data = json_decode(file_get_contents("php://input"), true);
 
@@ -17,8 +17,8 @@ if($data['username'] != "") {
     $username = $data['username'];
     $value = $data['value'];
 
-    $database->query("UPDATE Users SET gold = gold - :1 WHERE username = :0;", array($username, $value));
-    $gold = $database->query("SELECT gold FROM Users WHERE username = :0;", array($username));
+    $database->update_field('Userdata',array('username'=>$username),array('$inc'=>array('gold'=>-$value)));
+    $gold = $database->find_document('Userdata',array('username'=>$username),array('$projection'=>array('$_id'=>0, 'gold'=>1)));
 
     echo json_encode($gold[0]);
 }
