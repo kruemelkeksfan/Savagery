5d4c76711c9b920ae9762642c9c01f0608f68bd3
naomig96 <a01506542>
2020-06-09 01:39:13 +0200

    mongo set workers

--- api/Mongo/Buildings/post_set_workers.php
+++ api/Mongo/Buildings/post_set_workers.php
@@ -6,9 +6,9 @@ header("Access-Control-Max-Age: 3600");
 header("Access-Control-Allow-Headers: Content-Type, Access-Control-Allow-Headers, Authorization, X-Requested-With");
 
 
-include_once '../Database.php';
+include_once '../../MongoDatabase.php';
 
-$database = new Database();
+$database = new MongoDatabase();
 
 $data = json_decode(file_get_contents("php://input"), true);
 
@@ -17,13 +17,9 @@ if($data['username'] != "" && $data['building_id'] != "" && !empty($data['worker
     $building_id = $data['building_id'];
     $workers = $data['workers'];
 
-    $database->query("UPDATE Buildings
-		INNER JOIN Towns ON Buildings.town=Towns.townname
-		SET Buildings.workers=:2 WHERE Towns.owner=:0 AND Buildings.building_id=:1;", array($data['username'], $building_id, $workers));
+    $database->update_field('Userdata',array('username'=>$username),array('$set'=>array('buildings.$[id].workers'=>$workers)), array('arrayFilters'=>[array('id.building_id'=>$building_id)]));
 
-    $deployed_workers = $database->query("SELECT Buildings.workers FROM Buildings
-		INNER JOIN Towns ON Buildings.town=Towns.townname
-		WHERE Towns.owner=:0 AND Buildings.building_id=:1;", array($data['username'], $building_id));
+    $deployed_workers = $database->find_document('Userdata',array('username'=>$username),array('$projection'=>array('$_id'=>0, 'buildings'=>array('$elemMatch'=>array('building_id'=>$building_id)))));
 
     echo json_encode($deployed_workers[0]);
 
